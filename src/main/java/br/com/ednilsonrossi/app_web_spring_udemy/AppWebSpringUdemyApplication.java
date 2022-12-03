59fcf7e6edd7d1ab0d3b4bb77908a1cbf9d6ffd4
Ednilson Rossi <ednilsonrossi@gmail.com>
2022-12-03 17:56:05 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/ednilsonrossi/app_web_spring_udemy/AppWebSpringUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.ednilsonrossi.app_web_spring_udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebSpringUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebSpringUdemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ednilsonrossi/app_web_spring_udemy/AppWebSpringUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ednilsonrossi.app_web_spring_udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebSpringUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
