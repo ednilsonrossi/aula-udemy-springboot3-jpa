94bc01540f7c5cef22ad59521f12c7d5001b04df
Ednilson Rossi <ednilsonrossi@gmail.com>
2022-12-06 19:43:58 -0300

    Criado o enum e configurado para armazenar dados de forma correte no BD (inteiro). Foi alterado a classe Order para armazenar o enum no formato inteiro, contudo os objetos ainda não do tipo enum.

--- src/main/java/br/com/ednilsonrossi/app_web_spring_udemy/config/TestConfig.java
+++ src/main/java/br/com/ednilsonrossi/app_web_spring_udemy/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import br.com.ednilsonrossi.app_web_spring_udemy.model.Order;
 import br.com.ednilsonrossi.app_web_spring_udemy.model.User;
+import br.com.ednilsonrossi.app_web_spring_udemy.model.enums.OrderStatus;
 import br.com.ednilsonrossi.app_web_spring_udemy.repositories.OrderRepository;
 import br.com.ednilsonrossi.app_web_spring_udemy.repositories.UserRepository;
 
@@ -31,9 +32,9 @@ public class TestConfig implements CommandLineRunner{
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
--- src/main/java/br/com/ednilsonrossi/app_web_spring_udemy/model/Order.java
+++ src/main/java/br/com/ednilsonrossi/app_web_spring_udemy/model/Order.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat;
 
+import br.com.ednilsonrossi.app_web_spring_udemy.model.enums.OrderStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -30,14 +31,17 @@ public class Order implements Serializable{
 	private Instant moment;
 	
 	
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -67,6 +71,16 @@ public class Order implements Serializable{
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(this.orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
