c96636ddb9b153a2f324220a528eea33e152967f
Ednilson Rossi <ednilsonrossi@gmail.com>
2022-12-10 11:17:37 -0300

    Em payment criada a relação 1 para 1 com o mapeamento para o JPA.

--- src/main/java/br/com/ednilsonrossi/app_web_spring_udemy/model/Order.java
+++ src/main/java/br/com/ednilsonrossi/app_web_spring_udemy/model/Order.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat;
 
 import br.com.ednilsonrossi.app_web_spring_udemy.model.enums.OrderStatus;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -43,6 +45,9 @@ public class Order implements Serializable{
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items;
 
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+	
 	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.items = new HashSet<>();
@@ -52,6 +57,7 @@ public class Order implements Serializable{
 		this.client = client;
 	}
 
+	
 	public Order() {
 		super();
 	}
@@ -90,6 +96,17 @@ public class Order implements Serializable{
 		}
 	}
 	
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
+
 	public Set<OrderItem> getItems(){
 		return this.items;
 	}
