df3ebfabdbb0aa5a8455ea3f862988418738f4a8
Ednilson Rossi <ednilsonrossi@gmail.com>
2022-12-19 18:05:16 -0300

    tratamento de exceão no update

--- src/main/java/br/com/ednilsonrossi/app_web_spring_udemy/services/UserService.java
+++ src/main/java/br/com/ednilsonrossi/app_web_spring_udemy/services/UserService.java
@@ -12,6 +12,7 @@ import br.com.ednilsonrossi.app_web_spring_udemy.model.User;
 import br.com.ednilsonrossi.app_web_spring_udemy.repositories.UserRepository;
 import br.com.ednilsonrossi.app_web_spring_udemy.services.exceptions.DatabaseException;
 import br.com.ednilsonrossi.app_web_spring_udemy.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
 
 @Service
@@ -44,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User user) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, user);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, user);
+			return userRepository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User user) {
